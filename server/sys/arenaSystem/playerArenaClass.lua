521fa8adc8a7069886214b0cfd70270564f5bc3c
CommanderDev <57833245+CommanderDev@users.noreply.github.com>
2020-07-19 19:16:25 -0400

    Create playerArenaClass.lua

---
+++ server/sys/arenaSystem/playerArenaClass.lua
@@ -0,0 +1,11 @@
+local playerArenaClass = {}
+playerArenaClass.__index = playerArenaClass 
+
+playerArenaClass.roundBegun = _G.Event.new() --Event to fire to a players for every new round.
+
+playerArenaClass.new(playerObject, team, teamNumber) --Creates a bew player arena class. Arguments are the player object, the team name(typically team 1 or team 2) and the number the player is on the team. So first on the pad is 1, second is 2, etc.
+    local self = setmetatable({}, playerArenaClass)
+    return self
+end
+
+return playerArenaClass
