29c760fa465d28c8d875a1d5283c184eb9ff87dd
CommanderDev <57833245+CommanderDev@users.noreply.github.com>
2020-07-20 18:13:02 -0400

    Update Player.lua

--- server/sys/playerSystem/Player.lua
+++ server/sys/playerSystem/Player.lua
@@ -3,13 +3,11 @@ Player.__index = Player
 
 function Player.new(playerObject)
     local self = setmetatable({}, Player)
-    self.playerObject = playerObject
-    print(playerObject.Name.." CLass created")
+    
     return self
 end
 
 function Player:Destroy()
-    print(self.playerObject.Name.." class destroyed")
     self = nil
 end
 
