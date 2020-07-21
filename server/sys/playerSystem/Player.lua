def453230ef2f11c02a9d5b9af4c5e48fb88f44f
CommanderDev <57833245+CommanderDev@users.noreply.github.com>
2020-07-21 16:50:09 -0400

    Update Player.lua

--- server/sys/playerSystem/Player.lua
+++ server/sys/playerSystem/Player.lua
@@ -1,10 +1,25 @@
+---[[ Services ]]---
+local PhysicsService = game:GetService("PhysicsService")
 local Player = {}
 Player.__index = Player
 
+PhysicsService:CreateCollisionGroup("Players")
+PhysicsService:CollisionGroupSetCollidable("Players", "Players",false)
 function Player.new(playerObject)
     local self = setmetatable({}, Player)
-    
+    self.playerObject = playerObject
+    self:HandleCharacter()
     return self
+end
+
+function Player:HandleCharacter()
+    self.playerObject.CharacterAppearanceLoaded:Connect(function(characterObject)
+        for index, characterPart in next, characterObject:GetDescendants() do
+            if(characterPart:IsA("BasePart")) then
+                PhysicsService:SetPartCollisionGroup(characterPart, "Players")
+            end
+        end
+    end)
 end
 
 function Player:Destroy()
