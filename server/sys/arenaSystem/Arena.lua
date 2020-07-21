cc865a75476e779a10f11904b721225de858a710
CommanderDev <57833245+CommanderDev@users.noreply.github.com>
2020-07-21 13:15:44 -0400

    Update Arena.lua

--- server/sys/arenaSystem/Arena.lua
+++ server/sys/arenaSystem/Arena.lua
@@ -40,14 +40,17 @@ function Arena:DeterminePadActivation()
         coroutine.wrap(function()
             local start, finish = string.find(pad.Name, "-")
             local team = string.sub(pad.Name, 1, start-1)
+            local currentInTeam = self.teamCount[team]
             local padNumber = string.sub(pad.Name, finish+1, string.len(pad.Name))
             padNumber = tonumber(padNumber)
-            if(padNumber <= self.teamCount[team]+1) then
-                self.padFields[pad]:Start()
-                pad.Transparency = 0
-            else
+            if(padNumber > currentInTeam+1) then
                 self.padFields[pad]:Stop()
                 pad.Transparency = 1
+                pad.BrickColor = arenaData.inactiveBrickColor
+
+            else
+                self.padFields[pad]:Start()
+                pad.Transparency = 0
             end
         end)()
     end
@@ -57,26 +60,35 @@ function Arena:HandleEnteringAndLeavingPad(padObject, padField)
     local start, finish = string.find(padObject.Name, "-")
     local team = string.sub(padObject.Name, 1, start-1)
     local padNumber = string.sub(padObject.Name, finish+1, string.len(padObject.Name))
+    padNumber = tonumber(padNumber)
     padField.PlayerEntered:Connect(function(playerObject)
+        print("Player entered pad")
         if(self.padOwners[team][padNumber]) then return end
+        print("Pad number is true")
         local success, err = pcall(function()
-            padObject.BrickColor = arenaData.defaultTeamColors[team]
-            self.teamCount[team]  = padNumber
-            self.padOwners[team][padNumber] = playerObject
-           self:DeterminePadActivation()
+            if(self.teamCount[team] == padNumber - 1) then
+                padObject.BrickColor = arenaData.defaultTeamColors[team]
+                self.teamCount[team] = padNumber
+                self.padOwners[team][padNumber] = playerObject
+                self:DeterminePadActivation()
+            end
         end)
         if(not success) then
             print(err)
         end
+        print(self.teamCount[team].." is the number in "..team)
     end)
-    padField.PlayerLeft:Connect(function()
+    padField.PlayerLeft:Connect(function(playerObject)
+        if(self.padOwners[team][padNumber] ~= playerObject) then return end
+        self.padOwners[team][padNumber] = nil
+        if(padNumber > self.teamCount[team]) then return end
         padObject.BrickColor = arenaData.inactiveBrickColor
-        self.teamCount[team] = tonumber(padNumber-1)
+        self.teamCount[team] = padNumber - 1
+        
         if(self.teamCount[team]) < 0 then
             self.teamCount[team] = 0
         end
         print(self.teamCount[team].." is the amount on "..team)
-        self.padOwners[team][padNumber] = playerObject
         self:DeterminePadActivation()
     end)
 end
