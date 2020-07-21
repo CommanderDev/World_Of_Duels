cd32adce2a78837c13ae0c835fc4ff13c2ff897f
CommanderDev <57833245+CommanderDev@users.noreply.github.com>
2020-07-21 17:55:23 -0400

    Update Player.lua

--- server/sys/playerSystem/Player.lua
+++ server/sys/playerSystem/Player.lua
@@ -2,25 +2,37 @@
 local PhysicsService = game:GetService("PhysicsService")
 local Player = {}
 Player.__index = Player
+Player.playerClasses = {}
+
 
 PhysicsService:CreateCollisionGroup("Players")
+PhysicsService:CreateCollisionGroup("In Arena")
+
 PhysicsService:CollisionGroupSetCollidable("Players", "Players",false)
 function Player.new(playerObject)
     local self = setmetatable({}, Player)
     self.playerObject = playerObject
+    self.characterCollision = true --Determines if character collision is on or off.
     self:HandleCharacter()
+    Player.playerClasses[playerObject] = self
     return self
 end
 
 function Player:HandleCharacter()
     self.playerObject.CharacterAppearanceLoaded:Connect(function(characterObject)
-        for index, characterPart in next, characterObject:GetDescendants() do
-            if(characterPart:IsA("BasePart")) then
-                PhysicsService:SetPartCollisionGroup(characterPart, "Players")
-            end
-        end
+        self:SetCharacterCollisionGroup("Players")
     end)
 end
+
+function Player:SetCharacterCollisionGroup(collisionGroup, isCollission)
+    local characterObject = self.playerObject.Character or self.playerObject.CharacterAppearanceLoaded:Wait()
+    for index, characterPart in next, characterObject:GetDescendants() do
+        if(characterPart:IsA("BasePart")) then
+            PhysicsService:SetPartCollisionGroup(characterPart, collisionGroup)
+        end
+    end
+end
+
 
 function Player:Destroy()
     self = nil
