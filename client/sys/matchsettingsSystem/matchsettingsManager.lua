5cc47159f1c15e087d0198ddff0c208c21e85c5b
CommanderDev <57833245+CommanderDev@users.noreply.github.com>
2020-07-22 13:36:04 -0400

    Create matchsettingsManager.lua

---
+++ client/sys/matchsettingsSystem/matchsettingsManager.lua
@@ -0,0 +1,11 @@
+local matchsettingsManager = {}
+
+function matchsettingsManager:connect()
+    --Placeholder
+end 
+
+function matchsettingsManager:init()
+    self:connect()
+end 
+
+return matchsettingsManager
